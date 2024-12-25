0e457efb9781342754affe68a4d72a09b8e23a23
Mukul Chandrakant Mahadik <mukulm2010@gmail.com>
2024-12-25 10:09:29 -0800

    Added custom PUT/DELETE endpoints by overriding generic class-based views

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
@@ -8,3 +9,19 @@ from .serializers import BlogPostSerializer
 class BlogPostListCreate(generics.ListCreateAPIView):
 	queryset = BlogPost.objects.all()
 	serializer_class = BlogPostSerializer
+
+	def get(self, request, *args, **kwargs):
+		print("GET request received - getting all blog posts")
+		queryset = BlogPost.objects.all()
+		serializer = BlogPostSerializer(queryset, many=True)
+		return Response(serializer.data, status=status.HTTP_200_OK)
+
+	def delete(self, request, *args, **kwargs):
+		print("DELETE request received - deleting all blog posts")
+		BlogPost.objects.all().delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+	queryset = BlogPost.objects.all()
+	serializer_class = BlogPostSerializer
+	lookup_field = "pk"
